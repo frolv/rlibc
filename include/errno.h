6c786011dad9130bb2f577d4529d3babea159072
Alexei Frolov <alexeifrolov3@gmail.com>
2022-01-16 13:15:20 -0800

    errno: Add errno.h
    This adds the standard header <errno.h> and defines a few standard errno
    codes. Additionally, it expands the base test class with a method to
    assert the value of errno.

---
+++ include/errno.h
@@ -0,0 +1,52 @@
+// Copyright 2022 Alexei Frolov
+//
+// Use of this source code is governed by an MIT-style license
+// that can be found in the LICENSE file in the repository root.
+
+#ifndef RLIBC_ERRNO_H
+#define RLIBC_ERRNO_H
+
+#include <rlibc.h>
+
+#define EPERM   1
+#define ENOENT  2
+#define ESRCH   3
+#define EINTR   4
+#define EIO     5
+#define ENXIO   6
+#define E2BIG   7
+#define ENOEXEC 8
+#define EBADF   9
+#define ECHILD  10
+#define EAGAIN  11
+#define ENOMEM  12
+#define EACCES  13
+#define EFAULT  14
+#define ENOTBLK 15
+#define EBUSY   16
+#define EEXIST  17
+#define EXDEV   18
+#define ENODEV  19
+#define ENOTDIR 20
+#define EISDIR  21
+#define EINVAL  22
+#define ENFILE  23
+#define EMFILE  24
+#define ENOTTY  25
+#define ETXTBSY 26
+#define EFBIG   27
+#define ENOSPC  28
+#define ESPIPE  29
+#define EROFS   30
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+extern __thread int errno;
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif  // RLIBC_ERRNO_H
--- tests/rlibc_test.py
+++ tests/rlibc_test.py
@@ -27,3 +27,21 @@ class RlibcTest(unittest.TestCase):
             raise RuntimeError('No rlibc library found at %s' % lib)
 
         cls._rlibc = ctypes.cdll.LoadLibrary(lib)
+
+    class _ErrnoContext:
+        """Asserts that errno is set to a value following an operation."""
+
+        def __init__(self, test_case, expected: int):
+            self._test_case = test_case
+            self._errno = ctypes.c_int.in_dll(self._test_case._rlibc, 'errno')
+            self._expected = expected
+
+        def __enter__(self):
+            self._errno.value = 0
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self._test_case.assertEqual(self._errno.value, self._expected)
+            self._errno.value = 0
+
+    def assertErrno(self, value: int) -> None:
+        return RlibcTest._ErrnoContext(self, value)
