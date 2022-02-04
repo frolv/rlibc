ca9495db813c9bfef0b53faabdab7d9ceacd7497
Alexei Frolov <alexeifrolov3@gmail.com>
2022-02-04 05:04:09 -0800

    x86: Implement efficient copy and set operations
    This adds x86 implementations of the __rc_copy_bytes_fwd and
    __rc_set_bytes target functions.

--- target/x86/include/rlibc/target/memory.h
+++ target/x86/include/rlibc/target/memory.h
@@ -8,13 +8,36 @@
 
 #define __RLIBC_WORDSIZE 32
 
-// The compiler optimizes byte copying operations well on x86, so there's no
-// need to override them.
-#define __RLIBC_GENERIC_COPY_BYTES_FWD
 #define __RLIBC_GENERIC_COPY_BYTES_BWD
-
-#define __RLIBC_GENERIC_SET_BYTES
 #define __RLIBC_GENERIC_SCAN_BYTE
 #include <rlibc/memory_generic.h>
+
+#define __RLIBC_HAS_COPY_BYTES_FWD 1
+static inline void __rc_copy_bytes_fwd(uint8_t *dst,
+                                       const uint8_t *src,
+                                       size_t n)
+{
+    int a, b, c;
+
+    if (n > 0) {
+        __asm__ volatile("rep movsb"
+                         : "=&c"(a), "=&D"(b), "=&S"(c)
+                         : "0"(n), "1"(dst), "2"(src)
+                         : "memory");
+    }
+}
+
+#define __RLIBC_HAS_SET_BYTES 1
+static inline void __rc_set_bytes(uint8_t *dst, uint8_t c, size_t n)
+{
+    int a, b;
+
+    if (n > 0) {
+        __asm__ volatile("rep stosb"
+                         : "=&c"(a), "=&D"(b)
+                         : "a"(c), "1"(dst), "0"(n)
+                         : "memory");
+    }
+}
 
 #endif  // RLIBC_RLIBC_TARGET_MEMORY_H
